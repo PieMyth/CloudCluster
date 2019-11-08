180dd30ccf5119ac8ac11a3a6ee234780c4319db
Ian Percy <ipercy@pdx.edu>
2019-11-07 16:55:51 -0800

    changing sub file

--- sub.py
+++ sub.py
@@ -1,3 +1,15 @@
+# import zipfile,fnmatch,os
+# import gzip,shutil
+# rootPath = r"C:\users\ianpe\desktop\csv_files"
+# pattern = 'listings.csv.gz'
+# for root, dirs, files in os.walk(rootPath):
+#     for filename in fnmatch.filter(files, pattern):
+#         print(os.path.join(root, filename))
+#         os.chdir(root)
+#         with gzip.open(filename,'rb') as f_in:
+#             with open(filename[:-3], 'wb') as f_out:
+#                 shutil.copyfileobj(f_in,f_out)
+#         # zipfile.ZipFile(os.path.join(root, filename)).extractall(os.path.join(root, os.path.splitext(filename)[0]))
 
 import os
 import shutil
@@ -14,3 +26,4 @@ for root, dirs, files in os.walk(src_dir):
             print(new_dst_file_name)
             os.rename(dst_file, new_dst_file_name)
             count += 1
+            # shutil.copy(os.path.join(root,new_dst_file_name), dst_dir)
