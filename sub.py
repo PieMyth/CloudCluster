4e9a9d199233d6f27f84800678c439d028686024
Ian Percy <ipercy@pdx.edu>
2019-11-13 15:24:52 -0800

    pushing reviews changes and additions

--- sub.py
+++ sub.py
@@ -1,7 +1,9 @@
+# This block will unzip any .gz files
+
 # import zipfile,fnmatch,os
 # import gzip,shutil
-# rootPath = r"C:\users\ianpe\desktop\csv_files"
-# pattern = 'listings.csv.gz'
+# rootPath = r"C:\users\ianpe\desktop\reviews"
+# pattern = 'reviews.csv.gz'
 # for root, dirs, files in os.walk(rootPath):
 #     for filename in fnmatch.filter(files, pattern):
 #         print(os.path.join(root, filename))
@@ -9,16 +11,17 @@
 #         with gzip.open(filename,'rb') as f_in:
 #             with open(filename[:-3], 'wb') as f_out:
 #                 shutil.copyfileobj(f_in,f_out)
-#         # zipfile.ZipFile(os.path.join(root, filename)).extractall(os.path.join(root, os.path.splitext(filename)[0]))
+        # zipfile.ZipFile(os.path.join(root, filename)).extractall(os.path.join(root, os.path.splitext(filename)[0]))
 
+# This block will move any of the target csvs to another directory (since all are at first stored in sub sub sub sub sub directories)
 import os
 import shutil
 count=0
-src_dir = "C:/users/ianpe/desktop/csv_files"
-dst_dir = "C:/users/ianpe/desktop/all_files"
+src_dir = "C:/users/ianpe/desktop/reviews"
+dst_dir = "C:/users/ianpe/desktop/reviews_csvs"
 for root, dirs, files in os.walk(src_dir):
     for f in files:
-        if f.endswith('.csv'):
+        if f.endswith('reviews.csv'):
             dst_file = os.path.join(root, f)
             new_file_name = str(count)+f
             new_dst_file_name = os.path.join(dst_dir, new_file_name)
